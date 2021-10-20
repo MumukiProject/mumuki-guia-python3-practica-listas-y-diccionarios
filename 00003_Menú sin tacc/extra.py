0e89d727832c459db09d46736f64437e5d121cb2
franco@mumuki.org <franco@mumuki.org>
2021-10-20 11:28:32 +0000

    Mumuki Export on 2021-10-20 11:28:32 +0000

--- 00003_Menú sin tacc/extra.py
+++ 00003_Menú sin tacc/extra.py
@@ -1,3 +1,5 @@
+ingredientes_sin_tacc = ["tomate", "aceitunas", "cebolla", "carne picada", "lentejas"]
+
 empanadas = {
   "nombre": "empanadas de carne",
   "ingredientes": [ "carne picada", "aceitunas", "cebolla", "tapa de empanadas" ]
