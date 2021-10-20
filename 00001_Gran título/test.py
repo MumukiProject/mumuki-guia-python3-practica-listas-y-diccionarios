9faa2eaeaa81db09c764e029706a3155206a01b1
franco@mumuki.org <franco@mumuki.org>
2021-10-20 11:20:49 +0000

    Mumuki Export on 2021-10-20 11:20:49 +0000

--- 00001_Gran título/test.py
+++ 00001_Gran título/test.py
@@ -1,4 +1,13 @@
 class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_titulo_para_el_menu_spaghetti_frutti_di_mare(self):
+    
+    self.assertEqual(
+      titulo_para_el_menu("spaghetti frutti di mare"), 
+      "Spaghetti Frutti Di Mare")
+      
+  def test_titulo_para_el_menu_empanadas_de_carne_cuchillo(self):
+    
+    self.assertEqual(
+      titulo_para_el_menu("empanadas de carne cuchillo"), 
+      "Empanadas De Carne Cuchillo")      
