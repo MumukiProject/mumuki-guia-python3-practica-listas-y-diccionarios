e1fb86fbdd0e0f9d88cf6e8e68b2b701bfe32a55
franco@mumuki.org <franco@mumuki.org>
2021-10-20 11:17:58 +0000

    Mumuki Export on 2021-10-20 11:17:58 +0000

---
+++ 00003_Menú sin tacc/extra.py
@@ -0,0 +1,9 @@
+empanadas = {
+  "nombre": "empanadas de carne",
+  "ingredientes": [ "carne picada", "aceitunas", "cebolla", "tapa de empanadas" ]
+}
+
+guiso_de_lentejas = {
+  "nombre": "guiso de lentejas",
+  "ingredientes": [ "tomate", "lentejas", "cebolla" ]
+}
