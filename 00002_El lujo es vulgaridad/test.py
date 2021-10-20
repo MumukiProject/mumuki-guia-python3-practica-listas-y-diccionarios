2536b0609cf0b742549216a48ae1d6578cf6a441
franco@mumuki.org <franco@mumuki.org>
2021-10-20 11:41:03 +0000

    Mumuki Export on 2021-10-20 11:41:03 +0000

--- 00002_El lujo es vulgaridad/test.py
+++ 00002_El lujo es vulgaridad/test.py
@@ -2,7 +2,7 @@ class Test(unittest.TestCase):
 
   def test_las_empanadas_son_sofisticadas(self):
     self.assertTrue(es_sofisticada(empanadas))
-    
+
   def el_guiso_de_lentejas_no_es_sofisticado(self):
     self.assertFalse(es_sofisticada(guiso_de_lentejas))  
     
